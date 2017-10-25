70fc2ee034a5e4a35bee57c823d5d7444776cf0d
MMalaravan <32977280+MMalaravan@users.noreply.github.com>
2017-10-25 18:26:25 +0530

    cooding rain

---
+++ Culculator/src/stu/bcas/java/MyCulculatorConsDemo.java
@@ -0,0 +1,7 @@
+package stu.bcas.java;
+public class MyCulculatorConsDemo {
+public static void main(String[] args) {
+	MyCalculatorCons cal = new MyCalculatorCons(100,50);
+	cal.printsummary();
+}
+}
